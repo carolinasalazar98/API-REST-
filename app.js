3b64eed60a53157bf221959ae91a6ea6f3c97dba
CAROLINA <dycaro750@gmail.com>
2022-04-03 19:20:25 -0500

    se crea version 1 de API

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.get('/api/v1/sabado/', function (req, res) {
+  res.send('HOLA SOY LA PETICION GET')
+})
+
+app.listen(3000, function(){
+    console.log("SERVIDOR ENCENDIDO")
+}) //Funcion
